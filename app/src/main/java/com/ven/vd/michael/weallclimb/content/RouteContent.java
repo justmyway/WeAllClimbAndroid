f02780e62096f1977f70cf82111a8682b6730d85
Michael van de Ven <michael.vd.ven95@gmail.com>
2016-06-22 13:52:11 +0200

    Setup

---
+++ app/src/main/java/com/ven/vd/michael/weallclimb/content/RouteContent.java
@@ -0,0 +1,25 @@
+package com.ven.vd.michael.weallclimb.content;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Michael on 21-6-2016.
+ */
+public class RouteContent {
+    public static final List<Route> ROUTES = new ArrayList<~>();
+
+    public static final Map<String, Route> ROUTE_MAP = new HashMap<~>()
+
+    public static final int COUNT = 0;
+
+
+
+    public static class Route{
+        public Route(){
+
+        }
+    }
+}
